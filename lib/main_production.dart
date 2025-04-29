efe64e0440d930eff3c378a793b60923a7df23dd
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-04-29 22:30:17 +0100

    modified 9 files

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,8 +1,13 @@
 import 'package:freedom_driver/app/app.dart';
 import 'package:freedom_driver/bootstrap.dart';
 import 'package:freedom_driver/core/config/environment_config.dart';
+import 'package:freedom_driver/core/di/locator.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
   EnvironmentConfig.setEnvironment(Environment.production);
-  bootstrap(() => const App());
+  await locator();
+  await Hive.initFlutter();
+  await Hive.openBox<bool>('firstTimerUser');
+  await bootstrap(() => const App());
 }
