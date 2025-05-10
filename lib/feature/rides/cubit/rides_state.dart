3607cde0277488c22cd27cc27031192967aa0c28
Clever Akanimoh <cleverakanimoh02@gmail.com>
2025-05-10 12:47:06 +0100

    modified rides_state.dart in cubit

--- lib/feature/rides/cubit/rides_state.dart
+++ lib/feature/rides/cubit/rides_state.dart
@@ -1,11 +1,10 @@
 part of 'rides_cubit.dart';
 
- sealed class RidesState extends Equatable {
-  const  RidesState();
+sealed class RidesState extends Equatable {
+  const RidesState();
 
   @override
   List<Object> get props => [];
-
 }
 
 class RidesInitial extends RidesState {}
