6e199e89d187aadf4f78106ed15f1810e1005f61
clever akanimoh <cleverakanimoh02@gmail.com>
2025-05-26 22:45:46 +0100

    modified wallet_screen.dart and pubspec.yaml

--- lib/feature/debt_financial_earnings/view/wallet_screen.dart
+++ lib/feature/debt_financial_earnings/view/wallet_screen.dart
@@ -5,6 +5,7 @@ import 'package:freedomdriver/feature/debt_financial_earnings/cubit/finance/fina
 import 'package:freedomdriver/feature/debt_financial_earnings/cubit/finance/financial_state.dart';
 import 'package:freedomdriver/feature/debt_financial_earnings/widgets/earnings_banner.dart';
 import 'package:freedomdriver/feature/debt_financial_earnings/widgets/utility.dart';
+import 'package:freedomdriver/feature/documents/driver_license/view/license_form.dart';
 import 'package:freedomdriver/feature/kyc/view/background_verification_screen.dart';
 import 'package:freedomdriver/shared/app_config.dart';
 import 'package:freedomdriver/shared/widgets/app_icon.dart';
@@ -21,19 +22,20 @@ class WalletScreen extends StatefulWidget {
 }
 
 class _WalletScreenState extends State<WalletScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController accountNumber = TextEditingController();
+  final TextEditingController bankCode = TextEditingController();
+  final TextEditingController accountName = TextEditingController();
+  final TextEditingController phoneNumber = TextEditingController();
+
   @override
   void initState() {
     context.read<FinancialCubit>().getWalletBalance(context);
     super.initState();
   }
 
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController licenseNumber = TextEditingController();
-  final TextEditingController classOfLicense = TextEditingController();
-    
   @override
   Widget build(BuildContext context) {
-
     return BlocBuilder<FinancialCubit, FinancialState>(
       builder: (context, state) {
         final finance = state is FinancialLoaded ? state.finance : null;
@@ -84,34 +86,44 @@ class _WalletScreenState extends State<WalletScreen> {
                   dialogType: DialogType.noHeader,
                   animType: AnimType.topSlide,
                   title: 'Mobile Money',
-                  desc: 'This feature is not available yet.',
-                  customHeader: DecoratedContainer(
-                    child: Form(key: _formKey, child: Column()),
+                  body: DecoratedContainer(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          buildField(
+                            'Momo Phone Number',
+                            phoneNumber,
+                            keyboardType: TextInputType.phone,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  showCloseIcon: true,
-                  btnOkIcon: Icons.add,
-                  btnCancelIcon: Icons.cancel,
-                  // btnOkColor: gradient1,
+                  btnOkColor: Colors.black,
                   autoDismiss: false,
                   onDismissCallback: (type) {
-                    if (type == DismissType.topIcon ||
-                        type == DismissType.btnCancel) {
-                      Navigator.of(context).pop();
-                    }
+                    Navigator.of(context).pop();
                   },
                   btnOkText: 'Add Mobile Money',
                   btnCancelText: 'Cancel',
                   btnOkOnPress: () {
-                    context.read<FinancialCubit>().updateBankDetails(
+                    context.read<FinancialCubit>().updateMomoDetails(
                       context,
-                      accountName: '',
-                      accountNumber: '',
-                      bankCode: '',
+                      phoneNumber: phoneNumber.text.trim(),
                     );
                   },
+                  btnCancelOnPress: () {}
                 ).show();
               },
-              onAddWalletTap: () {},
+              onAddWalletTap: () {
+                context.read<FinancialCubit>().updateBankDetails(
+                  context,
+                  accountName: accountName.text.trim(),
+                  accountNumber: accountNumber.text.trim(),
+                  bankCode: bankCode.text.trim(),
+                );
+              },
               padding: EdgeInsets.zero,
             ),
           ],
